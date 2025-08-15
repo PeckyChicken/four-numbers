e56f4cffecb3aa2e45c7c430ccae337450a30dae
Benjy <BenjyPeckChicken@gmail.com>
2025-08-15 14:38:28 +0100

    initial commit

---
+++ functions.py
@@ -0,0 +1,145 @@
+import winsound
+from functools import reduce
+from random import choice, randint, sample, seed, shuffle
+from typing import Literal
+
+import regex as re
+from py_expression_eval import Parser
+
+parser = Parser()
+
+ops = ["+","-","*","/"]
+
+def randints(a: int,b: int,*,k=1,duplicates=False) -> list[int]:
+
+    output = [randint(a,b) for _ in range(k)] if duplicates else sample(range(a, b + 1), k=k)
+    return output
+
+def check_one_operation(nums,target):
+    for num1 in nums:
+        for num2 in nums:
+            for op in ops:
+                if eval_nums(num1,num2,op) == target:
+                    return True
+    return False
+
+#Copied code from stack overflow to get the factors of a number
+def factors(n) -> set[int]:    
+    return set(reduce(list.__add__, 
+                      ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+def create_puzzle(target_min,target_max,nums_min,nums_max,num_nums) -> tuple[int, list]:
+    target = randint(target_min,target_max)
+    current_value = target
+    nums = []
+    solution = f" = {target}"
+    nums_remaining = num_nums
+    while nums_remaining > 0:
+        num = randint(nums_min,nums_max) * choice([-1,1])
+        if nums_remaining >= 3:
+            current_value += num
+            solution = (f" + {-num}" if num < 0 else f" - {num}") + solution
+        elif nums_remaining == 2:
+            valid_factors = [x for x in factors(current_value) if nums_min <= x <= nums_max]
+            num = choice(valid_factors)
+            current_value //= num
+            solution = f" * {num}" + solution
+        else:
+            num = current_value
+            if not(nums_min <= num <= nums_max):
+                #Program failed, run it again
+                return create_puzzle(target_min,target_max,nums_min,nums_max,num_nums)
+            current_value -= num
+            solution = f"{num}" + solution
+
+        nums.append(abs(num))
+        nums_remaining -= 1
+    return target, nums, solution
+
+def create_target(nums: list[int],min_range,max_range):
+    target = -1
+    counter = 0
+    while not min_range < target < max_range or target in nums or check_one_operation(nums,target):
+        if counter >= 50:
+            return -1, "Error"
+        self_ops = ops.copy()
+        numbers = sample(nums,k=len(nums))
+        total_steps = len(nums)-1
+        target = numbers[0]
+        solution = f"{target}"
+        for idx, number in enumerate(numbers[1:total_steps+1]):
+            operation = choice(self_ops)
+            if operation == "*":
+                self_ops.remove(operation)
+            if idx == total_steps-1 and "*" in self_ops:
+                operation = "*"
+                self_ops.remove(operation)
+            while operation == "/" and not (target/number).is_integer():
+                operation = choice(self_ops)
+            solution += f" {operation} {number}"
+            target = eval_nums(target,number,operation)
+        counter += 1
+    return int(target), solution
+
+def str_list(object=[]) -> list[str]:
+    return [str(i) for i in object]
+
+def int_list(object=[]) -> list[int]:
+    return [int(i) for i in object]
+
+def eval_nums(num1,num2,op):
+    if op == "+":
+        return num1 + num2
+    if op == "-":
+        return num1 - num2
+    if op == "*":
+        return num1 * num2
+    if op == "/":
+        return num1 / num2
+    if op == "%":
+        return num1 % num2
+
+def display_nums(nums,target):
+    winsound.PlaySound("load.wav",winsound.SND_ASYNC)
+
+    if len(nums) <= 1:
+        print(f"Your numbers are {nums[0]}. ",end="")
+    else:
+        print(f"Your numbers are {", ".join(str_list(nums[:-1]))} and {nums[-1]}. ",end="")
+    print(f"Your target is {target}.")
+
+def eval_expression(expression:str,allowed_nums:list) -> list[list[int]|int]|str:
+    for x,item in enumerate(expression,start=1):
+        if item in ops:
+            continue
+        if item in (" ","(",")"):
+            continue
+        if item.isnumeric():
+            #Just ignore the numbers for now, we're checking them properly later
+            continue
+        #After checking the allowed characters, anything left will be a disallowed character.
+        return f'Error, disallowed or unrecognised character "{item}" in position {x}.'
+    
+
+    nums = int_list(re.findall(r"\d+",expression))
+    check_nums = allowed_nums.copy()
+    for x,item in enumerate(nums):
+        if item not in check_nums:
+            error_msg = f"Error, you do not have the number {item}{f" after using {nums[x-1]}" if x-1 >= 0 else ""}. "
+            if len(allowed_nums) <= 1:
+                error_msg += f"Your numbers are {allowed_nums[0]}. "
+            else:
+                error_msg += f"Your numbers are {", ".join(str_list(allowed_nums[:-1]))} and {allowed_nums[-1]}. "
+            return error_msg
+        check_nums.remove(item)
+    
+    try:
+        answer = parser.parse(expression).evaluate({})
+    except Exception:
+        return "Error, failed to parse expression."
+    if not answer.is_integer():
+        return "Error, answer is not whole."
+    if answer < 0:
+        print("Answer was negative, flipped to be positive")
+        answer = abs(answer)
+    return nums, int(answer)
---
+++ main.py
@@ -0,0 +1,188 @@
+from functions import *
+from py_expression_eval import Parser
+import os
+
+min_range = 2
+max_range = 60
+
+num_nums = 4
+
+
+score = 0
+attempts = 0
+output = ""
+
+test_parser = Parser()
+
+mode = ""
+while mode not in ("1","2","levels","level","leveled","random","l","r"):
+    mode = input("Leveled mode (1) or random mode (2)?\n> ")
+    mode = mode.strip()
+
+if "level" in mode or mode in ("1","l"):
+    mode = "leveled"
+else:
+    mode = "random"
+
+help_text = f'''The aim of the game is to combine the numbers you have using the 4 classic mathematical operations (addition +, subtraction -, multiplication * and division /) to reach your target.
+Once you combine 2 numbers they get replaced by the resulting number. You can type "nums", "numbers" or "n" at any time to see your current numbers, or "target" to see your target.
+A number can only be used once, but it does not have to be used at all.
+If you are struggling, you can type "factors <number>" to find the factors of the number, or just "factors" to find the factors of the target number.
+If you want to test out some math without having the numbers, you can type "test " followed by your math.
+If a puzzle is too hard for you AND you are in random mode, you can type "skip" or "new" and it will be replaced. You are currently in {mode} mode.
+If you make a mistake you can type "undo" or "u" to undo it.
+You can always type "restart", "reset", "undoall" or simply just "r", to restart the puzzle.
+If things get too cluttered, type "c", "clear" or "cls" to clear the screen.
+Expressions may contain any of the numbers you have, parentheses and any of the 4 listed operations.
+You can do multiple expressions in one line by separating them with ",". You will be informed if something went wrong.
+You can always type "help" to get this list up again.
+'''
+
+mini_help_text = f'You can use + - * or / and may use each number at most once. You can separate expressions with ",".\nType {'"new" or "skip" to get a new puzzle, ' if mode == "random" else ""}"help" to learn more, "undo" to undo your last move, "restart" or "r" to restart the puzzle or "nums" to see your current numbers.\nType "factors" to see the factors of the target number, or "test <expression>" to try out some math.'
+if mode == "leveled":
+    try:
+        with open("puzzle_id.txt","r") as f:
+            puzzle_id = int(f.read())
+    except FileNotFoundError:
+        puzzle_id = 0
+while True:
+    if mode == "leveled":
+        seed(puzzle_id)
+        seeded = puzzle_id
+        print(f"Puzzle #{puzzle_id}")
+        
+        max_range = puzzle_id+10
+    history = []
+    
+    target = -1
+
+    while target == -1:
+        if mode == "leveled":
+            seed(seeded)
+        allowed_nums = sorted(randints(min_range,max_range,k=num_nums,duplicates=False))
+        target, solution = create_target(allowed_nums,min_range=num_nums*4,max_range=num_nums*10)
+        if target == -1 and mode == "leveled":
+            seeded += 0.001
+    
+    display_nums(allowed_nums,target)
+    if attempts == 0:
+        print(mini_help_text)
+    print("Go!")
+    while target not in allowed_nums: #Keep going until either player wins or gives up.
+        expressions = input("> ").strip().lower()
+        if len(expressions) == 0:
+            winsound.PlaySound("enter.wav",winsound.SND_ASYNC)
+            continue
+
+        if expressions in ("n","nums","numbers","num","number"):
+            winsound.PlaySound("enter.wav",winsound.SND_ASYNC)
+            display_nums(allowed_nums,target)
+            continue
+
+        if expressions in ("g","goal","target"):
+            winsound.PlaySound("enter.wav",winsound.SND_ASYNC)
+            print(f"Your target is {target}.")
+            continue
+
+        if expressions in ("skip","pass","stop","new","newgame","new game","next") and mode == "random":
+            winsound.PlaySound("enter.wav",winsound.SND_ASYNC)
+            print(f"The solution was {solution}. Starting a new game...")
+            break
+
+        if expressions in ("h","help"):
+            winsound.PlaySound("enter.wav",winsound.SND_ASYNC)
+            print(help_text)
+            continue
+
+        if expressions in ("restart","reset","undoall","undo all","startover","start over","r"):
+            print("Restarting puzzle...")
+            if mode == "leveled":
+                print(f"Puzzle #{puzzle_id}")
+            for move in history[::-1]:
+                allowed_nums.remove(move[1])
+                allowed_nums.extend(move[0])
+            allowed_nums.sort()
+            history.clear()
+            display_nums(allowed_nums,target)
+            winsound.PlaySound("restart.wav",winsound.SND_ASYNC)
+            continue
+        
+        if expressions.split()[0] in ("factors","f","factorise","factorize"):
+            if len(expressions.split()) > 1:
+                if expressions.split()[1].replace("-","").isnumeric():
+                    num = int(expressions.split()[1])
+                elif expressions.split()[1].replace("-","").replace(".","").isnumeric() and "." in expressions.split()[1]:
+                    winsound.PlaySound("error.wav",winsound.SND_ASYNC)
+                    print(f"Error, {expressions.split()[1]} is not an integer.")
+                    continue
+                else:
+                    num = target
+            else:
+                num = target
+            winsound.PlaySound("enter.wav",winsound.SND_ASYNC)
+            num_factors = sorted(list(factors(num)))
+            print(f"The factors of {num} are {", ".join(str_list(num_factors[:-1]))} and {num_factors[-1]}. ")
+            continue
+        
+        if expressions.startswith("test "):
+            winsound.PlaySound("success.wav",winsound.SND_ASYNC)
+            expression = expressions[5:]
+            try:
+                result = test_parser.parse(expression).evaluate({})
+            except Exception:
+                result = "ERROR"
+            print(f"TEST: {expression} evaluated to {result}")
+            continue
+
+        if expressions in ("c","cls","clear","clearscreen","clear screen"):
+            winsound.PlaySound("enter.wav",winsound.SND_ASYNC)
+            print("Clearing screen...")
+            os.system("cls")
+            display_nums(allowed_nums,target)
+            continue
+        
+        if expressions in ("u","undo","back"):
+            if len(history) < 1:
+                print("Nothing to undo.")
+                winsound.PlaySound("error.wav",winsound.SND_ASYNC)
+
+                continue
+            winsound.PlaySound("undo.wav",winsound.SND_ASYNC)
+            lastmove = history[-1]
+            allowed_nums.remove(lastmove[1])
+            allowed_nums.extend(lastmove[0])
+            allowed_nums.sort()
+            history.pop(-1)
+            print(f"{lastmove[1]} has been reverted to {", ".join(str_list(lastmove[0][:-1]))} and {lastmove[0][-1]}")
+            continue    
+
+        expressions = expressions.split(",")
+        for expression in expressions:
+            expression = expression.strip()
+            if expression.startswith(tuple(ops)):
+                if len(history) == 0:
+                    print("Error, dangling operation.")
+                    winsound.PlaySound("error.wav",winsound.SND_ASYNC)
+                    continue
+                expression = f"{history[-1][1]}{" " if expression[1] == " " else ""}" + expression
+            evaluation = eval_expression(expression,allowed_nums)
+            if type(evaluation) == str: #An error message has been returned
+                print(evaluation)
+                winsound.PlaySound("error.wav",winsound.SND_ASYNC)
+                continue
+            winsound.PlaySound("success.wav",winsound.SND_ASYNC)
+            nums, answer = evaluation
+            history.append(evaluation)
+            print(f"{expression} = {answer}")
+            for i in nums:
+                allowed_nums.remove(i)
+            allowed_nums.append(answer)
+            allowed_nums.sort()
+    if target in allowed_nums:
+        print(f"You successfully got {target}! Starting a new game...")  
+    attempts += 1
+    if mode == "leveled":
+        puzzle_id += 1
+        with open("puzzle_id.txt","w") as f:
+            f.write(str(puzzle_id))
+    print("-"*50)
