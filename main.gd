e97ed2faa604bff88d24817d9bdecb8d3f4d53c9
Benjy <BenjyPeckChicken@gmail.com>
2025-08-18 07:30:58 +0100

    FEATURE: Target added

--- main.gd
+++ main.gd
@@ -10,6 +10,20 @@ enum Tiles{
 }
 @onready var containers: Array[Control] = [$Equation/Expression,$Storage,$Operations,$Equation/Answer]
 
+var target = 30
+var target_tile: NumberTile
+@onready var NUMBER_TILE_SCENE: PackedScene = load("res://number_tile.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	create_target_tile()
+
+func create_target_tile():
+	for prev_tile in $Target/Symbols.get_children():
+		prev_tile.queue_free()
+	target_tile = NUMBER_TILE_SCENE.instantiate()
+	target_tile.type = Tiles.STATIC
+	target_tile.number = target
+	target_tile.expression = "Target"
+	
+	$Target/Symbols.add_child(target_tile)
