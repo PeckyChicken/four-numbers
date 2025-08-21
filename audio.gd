0ca8393657e1df6f7223a843939188c4b142ae32
Benjy <BenjyPeckChicken@gmail.com>
2025-08-21 17:13:51 +0100

    FEATURE: Multiple audio streams can now be played syncronously

--- audio.gd
+++ audio.gd
@@ -1,4 +1,4 @@
-extends AudioStreamPlayer2D
+extends Control
 
 var cache: Dictionary[String,AudioStream] = {}
 # Called when the node enters the scene tree for the first time.
@@ -9,10 +9,13 @@ func play_sound(sound:String,location:Vector2):
 	if sound not in cache:
 		cache[sound] = load("res://Sounds/%s.wav" % [sound])
 	
-	stream = cache[sound]
+	var player = AudioStreamPlayer2D.new()
+	add_child(player)
+	player.stream = cache[sound]
 	
-	position = location
-	play()
+	player.position = location
+	player.play()
+	player.finished.connect(player.queue_free)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
