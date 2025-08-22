9e07a0814487370cbfa0e5c523bcad89cea77f99
Benjy <BenjyPeckChicken@gmail.com>
2025-08-22 16:01:20 +0100

    FEATURE: All sounds are now loaded on startup.

--- audio.gd
+++ audio.gd
@@ -5,6 +5,32 @@ var cache: Dictionary[String,AudioStream] = {}
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	Events.PlaySound.connect(play_sound)
+	load_all_sounds()
+
+func load_all_sounds():
+	var sounds: Array[String] = _find_all_sounds("./Sounds")
+	for sound in sounds:
+		if sound.get_extension() == "import":
+			sound = sound.replace(".import","")
+		load_sound(sound)
+
+func _find_all_sounds(directory) -> Array[String]:
+	var dir = DirAccess.open(directory)
+	var sounds: Array[String] = []
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				sounds.append_array(_find_all_sounds(directory + "/" + file_name))
+			else:
+				sounds.append(file_name)
+			
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+	
+	return sounds
 
 func play_sound(sound:String,location:Vector2):
 	var player = AudioStreamPlayer2D.new()
@@ -17,6 +43,8 @@ func play_sound(sound:String,location:Vector2):
 	player.finished.connect(player.queue_free)
 
 func load_sound(sound:String):
+	if sound.ends_with(".ogg"):
+		sound = sound.replace(".ogg","")
 	if sound not in cache:
 		cache[sound] = load("res://Sounds/%s.ogg" % [sound])
 	
