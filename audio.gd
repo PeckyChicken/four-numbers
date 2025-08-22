352c112d5ef9fec93f81ffb9d6f9b40e5f8ecf96
Benjy <BenjyPeckChicken@gmail.com>
2025-08-22 15:50:27 +0100

    CLEANUP: Separate function for loading sounds

--- audio.gd
+++ audio.gd
@@ -7,17 +7,20 @@ func _ready() -> void:
 	Events.PlaySound.connect(play_sound)
 
 func play_sound(sound:String,location:Vector2):
-	if sound not in cache:
-		cache[sound] = load("res://Sounds/%s.ogg" % [sound])
-	
 	var player = AudioStreamPlayer2D.new()
 	add_child(player)
-	player.stream = cache[sound]
+	
+	player.stream = load_sound(sound)
 	
 	player.position = location
 	player.play()
 	player.finished.connect(player.queue_free)
+
+func load_sound(sound:String):
+	if sound not in cache:
+		cache[sound] = load("res://Sounds/%s.ogg" % [sound])
 	
+	return cache[sound]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
