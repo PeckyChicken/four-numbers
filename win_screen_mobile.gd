95abfacd11ebd3786df940847b3522b22857ed65
Benjy <BenjyPeckChicken@gmail.com>
2025-08-25 07:58:19 +0100

    BUG UPDATE #6: Text and win screen have been upscaled appropriately. Not tiles as of yet though.

--- win_screen_mobile.gd
+++ win_screen_mobile.gd
@@ -5,8 +5,9 @@ extends WinScreen
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	MAIN_SCENE = load("res://mobile.tscn")
+	super()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	super(delta)
