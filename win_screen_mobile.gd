88a159f7039ae2a8dc072518ba1a2e1843d66950
Benjy <BenjyPeckChicken@gmail.com>
2025-08-25 07:09:51 +0100

    Scale mobile win screen

--- win_screen_mobile.gd
+++ win_screen_mobile.gd
@@ -1,14 +1,12 @@
-class_name Mobile
-extends Root
-
+class_name WinScreenMobile
+extends WinScreen
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	mobile = true
-	WIN_SCENE = load("res://win_screen_mobile.tscn")
-	super()
+	MAIN_SCENE = load("res://mobile.tscn")
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	super(delta)
+	pass
